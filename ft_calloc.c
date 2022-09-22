00e529994fe8667c1fa9121911e393c097f792d8
Paloma Latas <platas@c3r12s3.42madrid.com>
2022-09-22 12:18:30 +0200

    calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: platas <platas@student.42madrid.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/14 16:24:30 by platas            #+#    #+#             */
-/*   Updated: 2022/09/21 11:44:22 by platas           ###   ########.fr       */
+/*   Updated: 2022/09/22 12:14:19 by platas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,9 @@ void	*ft_calloc(size_t count, size_t size)
 {
 	void	*temp;
 
-	temp = (void *)malloc(size * count);
+	temp = (void *) malloc(size * count);
 	if (!temp)
 		return (NULL);
 	ft_bzero(temp, size * count);
 	return (temp);
 }
-/*
-int main()
-{
-	ft_calloc(2, 3);
-}*/
