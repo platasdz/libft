f025563a235078947a4fcf6a603f3cf3e19a89ad
Paloma Latas <platas@c3r12s3.42madrid.com>
2022-09-22 12:08:00 +0200

    memcpy sin restrict

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,13 +6,13 @@
 /*   By: platas <platas@student.42madrid.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/13 15:21:54 by platas            #+#    #+#             */
-/*   Updated: 2022/09/21 13:04:37 by platas           ###   ########.fr       */
+/*   Updated: 2022/09/22 12:06:05 by platas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memcpy(void *restrict dest, const void *restrict src, size_t n)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	size_t	i;
 
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: platas <platas@student.42madrid.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/13 09:44:23 by platas            #+#    #+#             */
-/*   Updated: 2022/09/20 15:10:44 by platas           ###   ########.fr       */
+/*   Updated: 2022/09/22 12:05:45 by platas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ int		ft_isdigit(int c);
 int		ft_isprint(int c);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
-void	*ft_memcpy(void *restrict dest, const void *restrict src, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_putchar_fd(char c, int fd);
