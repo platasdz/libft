8850aa624ea05db1154af1a90b835b6806b3ac52
Paloma Latas <platas@c3r12s4.42madrid.com>
2022-09-23 11:42:07 +0200

    corregido

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: platas <platas@student.42madrid.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/14 10:54:56 by platas            #+#    #+#             */
-/*   Updated: 2022/09/22 11:12:03 by platas           ###   ########.fr       */
+/*   Updated: 2022/09/23 11:38:28 by platas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,26 @@
 
 char	*ft_strrchr(const char *str, int c)
 {
-	size_t	len;
+	const char	*temp;
 
-	if (!str)
-		return (NULL);
-	len = ft_strlen(str);
-	while (len >= 0)
+	temp = str;
+	while (*str)
+		str++;
+	if (c == 0)
+		return ((char *)str);
+	while (temp <= str)
 	{
-		if (str[len] == (char)c)
-			return ((char *)str + len);
-		len--;
+		if (*str == (char)c)
+			return ((char *)str);
+		str--;
 	}
 	return (NULL);
 }
 /*
 int main()
 {
-	char *ret = ft_strrchr("rotijk", 'j');
-	char *rat = strrchr("rotijk", 'j');
+	char *ret = ft_strrchr("rotijk", 'z');
+	char *rat = strrchr("rotijk", 'z');
 	printf("The substring is: %s\n", ret);
 	printf("The substring is: %s\n", rat);
 }*/
