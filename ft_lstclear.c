05ceb2c74ffe99901bbbc3b26c41b57fd8b5f36a
Paloma Latas <platas@c3r12s6.42madrid.com>
2022-09-29 14:18:29 +0200

    protegido

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: platas <platas@student.42madrid.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 11:30:10 by platas            #+#    #+#             */
-/*   Updated: 2022/09/28 17:12:34 by platas           ###   ########.fr       */
+/*   Updated: 2022/09/29 14:05:34 by platas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
 	t_list	*aux;
 
+	if (!lst || !del)
+		return ;
 	while (*lst && lst)
 	{
 		aux = (*lst)->next;
