86f5831d3f66aff9208ddae1824dfd60f6822c27
Paloma Latas <platas@c3r12s6.42madrid.com>
2022-09-29 15:02:29 +0200

    corregido

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: platas <platas@student.42madrid.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/13 15:21:54 by platas            #+#    #+#             */
-/*   Updated: 2022/09/22 12:06:05 by platas           ###   ########.fr       */
+/*   Updated: 2022/09/29 15:01:55 by platas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	size_t	i;
 
-	i = 0;
-	if (!dest || !src)
+	if (!dest && !src)
 		return (NULL);
+	i = 0;
 	while (i < n)
 	{
 		*(unsigned char *)(dest + i) = *(unsigned char *)(src + i);
