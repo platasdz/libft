c0c2c23d3592a68af865dc9e8ea12ac46eae4315
Paloma Latas <platas@c3r12s6.42madrid.com>
2022-09-29 15:53:46 +0200

    corregido

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: platas <platas@student.42madrid.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 12:11:45 by platas            #+#    #+#             */
-/*   Updated: 2022/09/28 17:14:09 by platas           ###   ########.fr       */
+/*   Updated: 2022/09/29 15:53:18 by platas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,82 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 
 	if (!f || !lst)
 		return (NULL);
+	nl = NULL;
+	while (lst != NULL)
+	{
+		temp = ft_lstnew(f(lst->content));
+		if (!temp)
+		{
+			ft_lstclear(&nl, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&nl, temp);
+		lst = lst->next;
+	}
+	return (nl);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*nl;
+	t_list	*temp;
+
+	if (!f || !lst)
+		return (NULL);
 	temp = NULL;
 	while (lst != NULL)
 	{
 		nl = ft_lstnew(f(lst->content));
 		if (!nl)
+		{
 			ft_lstclear(&temp, del);
+			return (NULL);
+		}
 		ft_lstadd_back(&temp, nl);
 		lst = lst->next;
 	}
 	return (nl);
-}
+} */
